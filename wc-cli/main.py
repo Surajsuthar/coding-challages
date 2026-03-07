e20a9b562a6cd32b8ab238177463519da575afda
Suraj Suthar <surajsuthar0067@gmail.com>
2026-03-07 20:36:55 +0530

    first commit

---
+++ wc-cli/main.py
@@ -0,0 +1,82 @@
+import sys
+
+
+def count_bytes(filename: str):
+    try:
+        with open(filename, "rb") as f:
+            num_bytes = len(f.read())
+        return num_bytes
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+
+def count_lines(filename: str):
+    try:
+        with open(filename, "r") as f:
+            num_lines = sum(1 for _ in f)
+        return num_lines
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+
+def count_words(filename: str):
+    try:
+        with open(filename, "r") as f:
+            total_words = sum(1 for _ in f.read().split())
+        return total_words
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+
+def count_chars(filename: str):
+    try:
+        with open(filename, "r") as f:
+            num_chars = len(f.read())
+        return num_chars
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+
+
+def print_all(filename: str):
+    num_bytes = count_bytes(filename)
+    num_lines = count_lines(filename)
+    num_words = count_words(filename)
+    num_chars = count_chars(filename)
+    print(f"counting bytes: {num_bytes}")
+    print(f"counting lines: {num_lines}")
+    print(f"counting words: {num_words}")
+    print(f"counting characters: {num_chars}")
+
+    return num_bytes, num_lines, num_words, num_chars
+
+
+def main():
+    args = sys.argv[1]
+    print("args: ", args)
+    match args:
+        case "-c":
+            filename = sys.argv[2]
+            num_bytes = count_bytes(filename)
+            print(f"counting bytes: {num_bytes}")
+        case "-l":
+            filename = sys.argv[2]
+            num_lines = count_lines(filename)
+            print(f"counting lines: {num_lines}")
+        case "-w":
+            filename = sys.argv[2]
+            num_words = count_words(filename)
+            print(f"counting words: {num_words}")
+        case "-m":
+            filename = sys.argv[2]
+            num_chars = count_chars(filename)
+            print(f"counting characters: {num_chars}")
+        case "":
+            filename = sys.argv[2]
+            num_bytes, num_lines, num_words, num_chars = print_all(filename)
+            print("count -c, -l, -w, or -m")
+
+
+
+if __name__ == "__main__":
+    main()
